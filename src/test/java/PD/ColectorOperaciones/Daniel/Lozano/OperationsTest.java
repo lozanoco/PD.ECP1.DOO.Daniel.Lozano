8801f52e5f005f2592a69807c28c47db53bc647d
Daniel Lozano <lozanoco@gmail.com>
2014-10-16 18:04:44 +0200

    Añadido el test y verificado el funcionamiento

--- src/test/java/PD/ColectorOperaciones/Daniel/Lozano/OperationsTest.java
+++ src/test/java/PD/ColectorOperaciones/Daniel/Lozano/OperationsTest.java
@@ -6,5 +6,37 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class OperationsTest {
+	private OperationHandler operationHandler;
+	private Summation summation;
+	private Subtraction substraction;
+	private Multiplication multiplication;
+	
+	   @Before
+	    public void before() {
+		   this.operationHandler = new OperationHandler();
+		   this.summation = new Summation(1,2);
+		   this.substraction = new Subtraction(2,1);
+		   this.multiplication = new Multiplication(2,2);
+	    }
 
+	    @Test
+	    public void testSummation() {
+	        assertEquals(3, summation.calcular());
+	    }	
+	    
+	    @Test
+	    public void testSubstraction() {
+	        assertEquals(1, substraction.calcular());
+	    }	
+	    
+	    @Test
+	    public void testMultiplication() {
+	        assertEquals(4, multiplication.calcular());
+	    }	
+	    
+	    @Test
+	    public void testOperationHandler() {
+	    	this.operationHandler.add(this.summation);
+	        assertEquals(3, operationHandler.total());
+	    }	
 }
